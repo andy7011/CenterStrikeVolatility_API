3344c226d4d9d2db03a23b3d3b570d427b5966a9
ashadrin <ashadrin701@gmail.com>
2025-01-24 14:59:27 +0300

    Эксперименты с библиотекой Dash

--- CS_Volatility_Dash.py
+++ CS_Volatility_Dash.py
@@ -8,12 +8,12 @@ def zero_to_nan(values):
     return [float('nan') if x==0 else x for x in values]
 
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-# print(df.head())
+# print(df)
 
 # Указываем путь к файлу CSV
 fn = r'C:\Users\ashadrin\YandexDisk\_ИИС\Position\_TEST_CenterStrikeVola_RTS.csv'
 # Начальные параметры графиков: 840 - кол.торговых минуток за сутки
-limit_day = 1000
+limit_day = 100
 # Кол.торговых минуток за месяц 17640 = 840 мин x 21 раб. день
 limit_month = 17640
 
@@ -24,7 +24,7 @@ df = df.tail(limit_day)
 # Заменяем нули на NaN
 for i in range(1, len(df.columns)):
     df[df.columns[i]] = zero_to_nan(df[df.columns[i]])
-print(df)
+# print(df)
 
 # Преобразуем первую колонку в объект datetime
 df['DateTime'] = pd.to_datetime(df['DateTime'], dayfirst=True)
@@ -38,7 +38,7 @@ app.layout = html.Div([
         id='checklist',
         options=df.columns[1:11],
         value=df.columns[1:6].values,
-        inline=False),
+        inline=True),
 ])
 
 @callback(
@@ -47,7 +47,10 @@ app.layout = html.Div([
 
 def update_graph(value):
     # dff = df[df['W 30.01.2025']==value]
-    fig = px.line(df, x='DateTime', y='Q 20.03.2025')
+
+    fig = px.line(df, x='DateTime', y=[df.columns[1], df.columns[2], df.columns[3],
+                                       df.columns[4], df.columns[5], df.columns[6],
+                                       df.columns[7], df.columns[8], df.columns[9], df.columns[10]])
     fig.update_xaxes(
         rangebreaks=[
             dict(bounds=[0, 10], pattern="hour"),  # hide hours outside of 10am-0pm
