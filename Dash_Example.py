091594e16eb57e5ef3bc5b32e532db30c3b3f374
ashadrin <ashadrin701@gmail.com>
2025-02-20 17:42:55 +0300

    Эксперименты Dash 20.02.2025

--- Dash_Example.py
+++ Dash_Example.py
@@ -60,50 +60,34 @@ app.layout = html.Div(children=[
         dcc.Dropdown(df._base_asset_ticker.unique(), value=df._base_asset_ticker.unique()[0], id='dropdown-selection'),
         html.Div(id='dd-output-container')]),
 
-    html.Div(children=[
-        html.Div(children=[
-            dcc.Graph(id='plot')],
-            style={"border": "2px black solid"})],
-        style={'width': '100%', 'height': '90%', 'float': 'left', 'display': 'inline-block',
-                "border": "2px black solid"}),
-        dcc.Interval('interval-component', interval=1000 * 10, n_intervals=0),
-        html.Div(id='intermediate-value', children=df.to_json(date_format='iso', orient='split'),
+
+    html.Div(children=[dcc.Graph(id='plot')]),
+
+
+    dcc.Interval('interval-component', interval=1000 * 10, n_intervals=0),
+    html.Div(id='intermediate-value',
             style={'display': 'none'}),
 
     dash_table.DataTable(id='table', data=df_table.to_dict('records'), page_size=8)
 
 ])
 
-# # Callback to update the invisible intermediate-value element
-# @app.callback(Output('intermediate-value', 'children'),
-#               [Input('interval-component', 'n_intervals')],
-#               [State('intermediate-value', 'children')])
-# def clean_data(value):
-#     model_from_api = get_object_from_json_endpoint('https://option-volatility-dashboard.ru/dump_model')
-#
-#     # Список базовых активов, вычисление и добавление в словарь центрального страйка
-#     base_asset_list = model_from_api[0]
-#     for asset in base_asset_list:
-#         ticker = asset.get('_ticker')
-#         last_price = asset.get('_last_price')
-#         strike_step = MAP[ticker]['strike_step']
-#         central_strike = _calculate_central_strike(last_price, strike_step)  # вычисление центрального страйка
-#         asset.update({
-#             'central_strike': central_strike
-#         })
-#     # print('base_asset_list:', base_asset_list) # вывод списка базовых активов
-#
-#     # Список опционов
-#     option_list = model_from_api[1]
-#     current_datetime = datetime.datetime.now()
-#     for option in option_list:
-#         option['datetime'] = current_datetime
-#     df = pd.DataFrame.from_dict(option_list, orient='columns')
-#     df = df.loc[df['_volatility'] > 0]
-#     df['_expiration_datetime'] = pd.to_datetime(df['_expiration_datetime'])
-#     df['_expiration_datetime'].dt.date
-#     df['expiration_date'] = df['_expiration_datetime'].dt.strftime('%d.%m.%Y')
-#     return df
+# Callback to update the invisible intermediate-value element
+@app.callback(Output('intermediate-value', 'children'),
+              [Input('interval-component', 'n_intervals')],
+              [State('intermediate-value', 'children')])
+def clean_data(value, dff):
+    model_from_api = get_object_from_json_endpoint('https://option-volatility-dashboard.ru/dump_model')
+
+    # Список опционов
+    option_list = model_from_api[1]
+    df = pd.DataFrame.from_dict(option_list, orient='columns')
+    df = df.loc[df['_volatility'] > 0]
+    df['_expiration_datetime'] = pd.to_datetime(df['_expiration_datetime'])
+    df['_expiration_datetime'].dt.date
+    df['expiration_date'] = df['_expiration_datetime'].dt.strftime('%d.%m.%Y')
+    dff = df[(df._base_asset_ticker == 'RIH5') & (df._type == 'C')]
+    return dff.tail(100).to_json(date_format='iso', orient='split')
 
 @app.callback(
     Output('dd-output-container', 'children'),
@@ -133,11 +117,11 @@ def update_output(value):
 
     dff = df[(df._base_asset_ticker == 'RIH5') & (df._type == 'C')]
 
-    print(dff)
+    # print(dff)
 
     fig = px.line(dff, x='_strike', y='_volatility', color='expiration_date')
-    fig.update_xaxes(range=[dff._strike.min(), dff._strike.max()])
-    fig.update_layout(title_text="Volatility smile of the option series", uirevision="Don't change")
+    # fig.update_xaxes(range=[dff._strike.min(), dff._strike.max()])
+    # fig.update_layout(title_text="Volatility smile of the option series", uirevision="Don't change")
     return fig
 
 #Callback to update the table
