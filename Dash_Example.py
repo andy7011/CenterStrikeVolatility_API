0e3fa6238ad88f65d76e87936953bbd9bf048b81
Andy701 <ashadrin701@gmail.com>
2025-02-20 22:58:29 +0300

    Эксперименты с DataFrame 17.02.2025

--- Dash_Example.py
+++ Dash_Example.py
@@ -17,7 +17,7 @@ app = dash.Dash(external_stylesheets=external_stylesheets)
 # app = dash.Dash(__name__)
 
 # My positions data
-df_table = pd.read_csv('C:\\Users\\ashadrin\\YandexDisk\\_ИИС\\Position\\MyPos.csv', sep=';')
+df_table = pd.read_csv('C:\\Users\\Андрей\\YandexDisk\\_ИИС\\Position\\MyPos.csv', sep=';')
 
 def get_object_from_json_endpoint(url, method='GET', params={}):
     response = requests.request(method, url, params=params)
@@ -51,26 +51,30 @@ df = df.loc[df['_volatility'] > 0]
 df['_expiration_datetime'] = pd.to_datetime(df['_expiration_datetime'])
 df['_expiration_datetime'].dt.date
 df['expiration_date'] = df['_expiration_datetime'].dt.strftime('%d.%m.%Y')
+print(df.columns)
 
 app.layout = html.Div(children=[
 
     html.H6(id='last_update_time', style={'textAlign': 'left'}),
 
-    html.Div([
+    html.Div(children=[
         dcc.Dropdown(df._base_asset_ticker.unique(), value=df._base_asset_ticker.unique()[0], id='dropdown-selection'),
         html.Div(id='dd-output-container')]),
 
+        html.Div(children=[
+            dcc.Graph(id='plot')]),
 
-    html.Div(children=[dcc.Graph(id='plot')]),
+    dcc.Interval(
+        id='interval-component',
+        interval=1000 * 10,
+        n_intervals=0),
 
-
-    dcc.Interval('interval-component', interval=1000 * 10, n_intervals=0),
-    html.Div(id='intermediate-value',
-            style={'display': 'none'}),
-
-    dash_table.DataTable(id='table', data=df_table.to_dict('records'), page_size=8)
+    html.Div(id='intermediate-value', style={'display': 'none'}),
+        dash_table.DataTable(id='table', data=df_table.to_dict('records'), page_size=8)
 
 ])
+
+
 
 # Callback to update the invisible intermediate-value element
 @app.callback(Output('intermediate-value', 'children'),
@@ -86,9 +90,11 @@ def clean_data(value, dff):
     df['_expiration_datetime'] = pd.to_datetime(df['_expiration_datetime'])
     df['_expiration_datetime'].dt.date
     df['expiration_date'] = df['_expiration_datetime'].dt.strftime('%d.%m.%Y')
-    dff = df[(df._base_asset_ticker == 'RIH5') & (df._type == 'C')]
+    dff = df[(df._base_asset_ticker == value) & (df._type == 'C')]
+    # print(dff)
     return dff.tail(100).to_json(date_format='iso', orient='split')
 
+# Callback to update the dropdown
 @app.callback(
     Output('dd-output-container', 'children'),
     Input('dropdown-selection', 'value')
@@ -103,8 +109,9 @@ def update_time(n):
     return 'Last update time: {}'.format(datetime.datetime.now())
 
 #Callback to update the line-graph
-@app.callback(Output('plot', 'figure'),
-              [Input('intermediate-value', 'children')])
+@app.callback(Output('plot', 'figure', allow_duplicate=True),
+              Input('intermediate-value', 'children'),
+              prevent_initial_call=True)
 def update_output(value):
     model_from_api = get_object_from_json_endpoint('https://option-volatility-dashboard.ru/dump_model')
     # Список опционов
@@ -115,11 +122,12 @@ def update_output(value):
     df['_expiration_datetime'].dt.date
     df['expiration_date'] = df['_expiration_datetime'].dt.strftime('%d.%m.%Y')
 
-    dff = df[(df._base_asset_ticker == 'RIH5') & (df._type == 'C')]
+    dff = df[(df._base_asset_ticker == value) & (df._type == 'C')]
 
-    # print(dff)
+    print(dff)
 
     fig = px.line(dff, x='_strike', y='_volatility', color='expiration_date')
+    fig_last = px.line(dff, x='_strike', y='_volatility', color='expiration_date')
     # fig.update_xaxes(range=[dff._strike.min(), dff._strike.max()])
     # fig.update_layout(title_text="Volatility smile of the option series", uirevision="Don't change")
     fig.update_layout(
@@ -133,7 +141,7 @@ def update_output(value):
     Input('interval-component', 'n_intervals')
 )
 def updateTable(n):
-    df_pos = pd.read_csv('C:\\Users\\ashadrin\\YandexDisk\\_ИИС\\Position\\MyPos.csv', sep=';')
+    df_pos = pd.read_csv('C:\\Users\\Андрей\\YandexDisk\\_ИИС\\Position\\MyPos.csv', sep=';')
     return df_pos.to_dict('records')
 
 if __name__ == '__main__':
