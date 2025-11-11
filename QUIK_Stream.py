9d6ab081c21bb0d73f5516036b857d1a082c823d
ASHADRIN <ashadrin701@gmail.com>
2025-11-11 18:33:40 +0300

    Доработка 11.11.2025 QUIK_Stream.py

--- QUIK_Stream.py
+++ QUIK_Stream.py
@@ -29,11 +29,12 @@ if __name__ == '__main__':  # Точка входа при запуске это
     logger = logging.getLogger('QuikPy.Accounts')  # Будем вести лог
     qp_provider = QuikPy()  # Подключение к локальному запущенному терминалу QUIK
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Формат сообщения
-                        datefmt='%d.%m.%Y %H:%M:%S',  # Формат даты
-                        level=logging.DEBUG,  # Уровень логируемых событий NOTSET/DEBUG/INFO/WARNING/ERROR/CRITICAL
-                        handlers=[logging.FileHandler('QUIK_Stream.log', encoding='utf-8'), logging.StreamHandler()])  # Лог записываем в файл и выводим на консоль
-    logging.Formatter.converter = lambda *args: datetime.now(tz=qp_provider.tz_msk).timetuple()  # В логе время указываем по МСК
+    # # Закомментировать, чтобы не было логов
+    # logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Формат сообщения
+    #                     datefmt='%d.%m.%Y %H:%M:%S',  # Формат даты
+    #                     level=logging.DEBUG,  # Уровень логируемых событий NOTSET/DEBUG/INFO/WARNING/ERROR/CRITICAL
+    #                     handlers=[logging.FileHandler('QUIK_Stream.log', encoding='utf-8'), logging.StreamHandler()])  # Лог записываем в файл и выводим на консоль
+    # logging.Formatter.converter = lambda *args: datetime.now(tz=qp_provider.tz_msk).timetuple()  # В логе время указываем по МСК
 
     class_codes = qp_provider.get_classes_list()['data']  # Режимы торгов через запятую
     class_codes_list = class_codes[:-1].split(',')  # Удаляем последнюю запятую, разбиваем значения по запятой в список режимов торгов
