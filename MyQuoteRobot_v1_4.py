4652ee5f93ffa9ac2a72c30293b13bae67d25a31
ASHADRIN <ashadrin701@gmail.com>
2026-03-16 18:39:43 +0300

    Доработка 16.03.2026 Доработки

--- MyQuoteRobot_v1_4.py
+++ MyQuoteRobot_v1_4.py
@@ -2,7 +2,7 @@
 from MyQuoteRobot_Config import dataname_buy, dataname_sell, stop_iv_sell, stop_iv_buy, expected_profit, Lot_count, Timeout
 
 import logging # Выводим лог на консоль и в файл
-# logging.basicConfig(level=logging.WARNING) # уровень логгирования
+logging.basicConfig(level=logging.WARNING) # уровень логгирования
 from datetime import datetime, timezone  # Дата и время
 from zoneinfo import ZoneInfo
 from time import sleep  # Задержка в секундах перед выполнением операций
